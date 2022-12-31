3756738b191f59b9b982f96a15483348096e5b7a
Faith Adebayo <63503378+Dev-Faith@users.noreply.github.com>
2022-12-31 13:48:52 +0100

    wooos

--- comp/navbar.js
+++ comp/navbar.js
@@ -13,17 +13,17 @@ const Navbar = () => {
           <Image src="/logo.svg" alt="logo" height={27} width={93} />
         </div>
         <div className="bugger" onClick={() => setMenu(!menu)}>
-          {!menu ? <BsTextRight /> : <RxCross1 />}
+          {!menu ? <RxCross1 /> : <BsTextRight />}
         </div>
-        <div className={menu ? "menu off" : "menu"}>
-            <div className="menu-list">
-                <ul>
-                <li className="active">Home</li>
-                <li>Shop</li>
-                <li>Cart</li>
-                <li>Account</li>
-                </ul>
-            </div>
+        <div className={!menu ? "menu off" : "menu"}>
+          <div className="menu-list">
+            <ul>
+              <li className="active">Home</li>
+              <li>Shop</li>
+              <li>Cart</li>
+              <li>Account</li>
+            </ul>
+          </div>
         </div>
       </div>
     );
